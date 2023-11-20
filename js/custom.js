38b48cb4b7d1c551041dd9fc3cf23f6a63a5c0a3
Dang-Tuan <151108206+Dang-Tuan@users.noreply.github.com>
2023-11-20 11:50:01 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+ // JavaScript Document
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('#intro-chay').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	autoplay:true,
+	autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
